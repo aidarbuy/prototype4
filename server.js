bdd3c270f110d2174e8f5769bdcd466b2ab147e5
aidarbuy <aidarbuy@gmail.com>
2015-02-12 04:14:20 +0000

    root

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', __dirname);
+app.set('view engine', 'jade');
+
+
+app.get('*', function(req, res) {
+    res.render('index');
+});
+
+app.listen(process.env.PORT, process.env.IP);
